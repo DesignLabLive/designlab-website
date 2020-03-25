d109ea3257012cd00a73561e14cc4488e50f0608
Azmarie Wang <azmarie.yx@gmail.com>
2020-03-24 21:47:13 -0700

    update dependencies

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,12 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   return new Promise((resolve, reject) => {
-    const blogPost = path.resolve("./src/templates/blog-post.js")
+    const event = path.resolve("./src/templates/event-page.js")
     resolve(
       graphql(
         `
           {
-            allContentfulBlogPost {
+            allContentfulEvent {
               edges {
                 node {
                   title
@@ -26,11 +26,11 @@ exports.createPages = ({ graphql, actions }) => {
           reject(result.errors)
         }
 
-        const posts = result.data.allContentfulBlogPost.edges
+        const posts = result.data.allContentfulEvent.edges
         posts.forEach((post, index) => {
           createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
+            path: `/events/${post.node.slug}/`,
+            component: event,
             context: {
               slug: post.node.slug,
             },
